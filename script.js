8028a867ae1b5ced4fd07036e00786d833b93cd2
gautham8325 <gautham8325@gmail.com>
2024-08-13 13:31:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,176 @@
+var audio = new Audio('assets/sentmessage.mp3');
+var contactString = "<div class='social'> <a target='_blank' href='tel:+911234567890'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>1234567890</label></div> </a> <a href='mailto:gautham8325@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/gautham8325'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://whatsapp.com/channel/0029Val9nbkGehEIzkRbwo0U'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/gautham8325'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://www.instagram.com/gautham8325?igsh=dG5jb3U3b3VyOWVl'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://in.linkedin.com/in/gautham-k8325' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Gautham Resume.pdf</label></div><a href='assets/Gautham&#39;s Resume.pdf' download='Gautham_Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
+var addressString = "<div class='mapview'><iframe src='https://maps.google.com/maps?q=Sri+sai+srinivasa+park+view+apartment+JC7J+X46,+Padmavati+Nagar,+Tirupati,+Andhra+Pradesh+517501&output=embed' width='100%' height='450' frameborder='0' style='border:0' allowfullscreen></iframe></div><label class='add'><address>Sri Sai Srinivasa Park View Apartment<br>JC7J+X46, Padmavati Nagar<br>Tirupati, Andhra Pradesh 517501</address>";
+
+
+function startFunction() {
+    setLastSeen();
+    waitAndResponce("intro");
+}
+
+function setLastSeen() {
+    var date = new Date();
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
+}
+
+
+function closeFullDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+function openFullScreenDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+
+function isEnter(event) {
+    if (event.keyCode == 13) {
+        sendMsg();
+    }
+}
+
+function sendMsg() {
+    var input = document.getElementById("inputMSG");
+    var ti = input.value;
+    if (input.value == "") {
+        return;
+    }
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "sent");
+    greendiv.setAttribute("class", "green");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = input.value;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    setTimeout(function () { waitAndResponce(ti) }, 1500);
+    input.value = "";
+    playSound();
+}
+
+function waitAndResponce(inputText) {
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "typing...";
+    switch (inputText.toLowerCase().trim()) {
+        case "intro":
+            setTimeout(() => {
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Gautham K</a>.</span><br><br>I am a Computer Engineering student at <span class='bold'>SRM  University AP 👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+            }, 2000);
+            break;
+        case "help":
+            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            break;
+        case "resume":
+            sendTextMessage(resumeString);
+            break;
+        case "skills":
+            sendTextMessage("<span class='sk'>I am currently pursuing B.Tech degree in Computer Science Engineering.<br><br>Programming Languages  :<br><span class='bold'>Java<br>C++<br>C<br>PHP<br>Python<br><br></span>Also <span class='bold'><br>Problem Solving<br>Data Structures and Algorithms<br> AI & ML <br><br></span>Frontend:<span class='bold'><br>HTML<br>CSS<br>JavaScript<br>Tailwind CSS<br><React JS><br><br></span> Technologies:<br><span class='bold'>Linux<br>Windows<br><br></span>DataBase:<br><span class='bold'>Mysql<br><br></span>Favourite IDE:<br><span class='bold'>VSCode</span>");
+            break;
+
+        case "education":
+            sendTextMessage("I am currently pusuing B.Tech degree in Computer Science Engineering from SRM University<br>Passing Year : 2026<br><br>I have completed my Secondary school from Sri Chaitanya Junior College ,Raman Bhavan <br>Passing Year:2022");
+            break;
+
+        case "address":
+            sendTextMessage(addressString);
+            break;
+        case "clear":
+            clearChat();
+            break;
+        case "about":
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https://www.instagram.com/gautham8325?igsh=dG5jb3U3b3VyOWVl'><span class='bold'>Gautham K</a> with ❤️</span>");
+            break;
+        case "contact":
+            sendTextMessage(contactString);
+            break;
+        case "projects":
+            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/gautham8325'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            break;
+        case "new":
+            sendTextMessage(addressString);
+            break;
+        default:
+            setTimeout(() => {
+                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'help' to know more about usage.");
+            }, 2000);
+            break;
+    }
+
+
+
+}
+
+function clearChat() {
+    document.getElementById("listUL").innerHTML = "";
+    waitAndResponce('intro');
+}
+
+
+
+function sendTextMessage(textToSend) {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.setAttribute("id", "sentlabel");
+    dateLabel.id = "sentlabel";
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    greendiv.innerHTML = textToSend;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+
+function sendResponse() {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ";
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+function playSound() {
+    audio.play();
+}
